9e9264870c37e1b710a70c4e499948172584c44e
Zaahir Dawood <zaahirdawood@gmail.com>
2024-10-06 17:52:57 +0100

    update: db_prep.py and db.py copde

---
+++ src/ecom_bot/db_prep.py
@@ -0,0 +1,19 @@
+# import os
+# import requests
+# import pandas as pd
+# from tqdm.auto import tqdm
+from dotenv import load_dotenv
+
+from db import init_db
+
+load_dotenv()
+
+def main():
+    print("Initializing database...")
+    init_db()
+
+    print("Indexing process completed successfully!")
+
+
+if __name__ == "__main__":
+    main()
