4eb821a29992c28136eab79dee0c327bafa0df8a
Zaahir Dawood <zaahirdawood@gmail.com>
2024-10-07 19:34:07 +0100

    update: test.py script

---
+++ test.py
@@ -0,0 +1,15 @@
+import requests
+
+url='http://127.0.0.1:5000/question'
+
+question= (
+    "How can I find out where is my order?"
+)
+
+data= {'question':question}
+
+
+response= requests.post(url=url, json=data).json()
+
+
+print(response)
